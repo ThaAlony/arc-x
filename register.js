a73afe6767f447cfb135f5525dc527f472e3d600
alony <38983726+ThaAlony@users.noreply.github.com>
2021-08-09 18:15:31 +0200

    Register aggiornato con i ruoli booster e tester
    register privato che fa registrare solo chi ha il ruolo booster o beta tester

---
+++ register.js
@@ -0,0 +1,36 @@
+exports.run = (client, message, args, command, account) => {
+
+    if (!message.member.roles.cache.has(client.config.DiscordBoosterRole) && !message.member.roles.cache.has(client.config.MVOBetaTester) ) return message.reply(" not a beta tester")
+    // PRIVATE LIST
+
+    if (account) return message.reply(" you are already registered")
+    if (!args[0]) return message.reply(" the correct use is " + client.config.prefix + command + " nickname language")
+
+    if (!args[1] || (args[1].toLowerCase() != "en" && args[1].toLowerCase() != "it")) return message.reply(" supported languages : en ( english ) - it ( italiano )")
+    client.lingua = args[1].toLowerCase()
+
+    let i = -1;
+    let user;
+    do {
+        i++;
+        user = client.getUserRow.get(i)
+    } while (user)
+
+    let acc = {
+        "rowid": i,
+        "id": message.author.id,
+        "lingua": client.lingua,
+        "guildID": null,
+        "xp": 0,
+        "level": 0,
+        "rank": "Bronze",
+        "nickname": args[0],
+        "artwork": null,
+        "mbits": 0,
+        "boost": 0
+    }
+
+    client.setUser.run(acc);
+    message.reply(" registered ")
+
+}
