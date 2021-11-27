9b9e6a3af2be968f07fc929f8eb727a0760c0d83
alony <38983726+ThaAlony@users.noreply.github.com>
2021-11-27 14:10:05 +0100

    auto-account quando si usa un comando del bot e no more private list

--- events/message.js
+++ events/message.js
@@ -4,6 +4,10 @@ module.exports = (client, message) => {
 
     //  S T A R T I N G
 
+    
+
+
+
     if (message.guild.id != client.config.GuildServerID) return
     let account = client.getUser.get(message.author.id)
     if (account) {
@@ -19,6 +23,33 @@ module.exports = (client, message) => {
     const cmd = client.commands.get(command);
 
     if (!cmd) return;
+
+    if (!account) {
+
+        let i = -1;
+        let user;
+        do {
+            i++;
+            user = client.getUserRow.get(i)
+        } while (user)
+
+        account = {
+            "rowid": i,
+            "id": message.author.id,
+            "lingua": client.lingua,
+            "guildID": null,
+            "xp": 0,
+            "level": 0,
+            "rank": "Bronze",
+            "nickname": message.author.nickname,
+            "artwork": null,
+            "mbits": 0,
+            "boost": 0,
+            "character": "male-/char_skin-- male-/char_outline-- eyes-/eyes_1-- eyes-/eyes_1_pupils-black- hairs-/hair_1_chroma-brown-1 hairs-/hair_1_outline- outfit-/outfit_1--"
+        }
+        client.setUser.run(acc);
+        message.reply(" you are now registered, try mv infos and remember to customize your character!")
+    }
 
     cmd.run(client, message, args, command, account);
 };
