8f65b4245f6a9be2d881bda43362efbdcd60e9dc
alony <38983726+ThaAlony@users.noreply.github.com>
2021-11-29 23:51:14 +0100

    update grafico

--- commands/help.js
+++ commands/help.js
@@ -1,24 +1,24 @@
-exports.run = (client, message, args, command, account) => {
+﻿exports.run = (client, message, args, command, account) => {
     
     const Discord = require("discord.js")
 
     let embed
     if (client.lingua == "it") {
         embed = new Discord.MessageEmbed()
-            .setTitle("Un po' di Aiuto?")
-            .addField("Cosa?", "**AW | Project X** � il centro di MVO, non solo lavora nel server dicord principale ma gestisce il sistema di gilde")
-            .addField("Chi?", "Il Bot � stato creato e viene gestito da `" + client.guilds.cache.get(client.config.GuildServerID).members.cache.get(client.config.aloneID).user.tag + "`")
-            .addField("Altro?", "Queste info sono state scritte il mese di Gennaio 2021 ed il Bot era in Pre Alpha, \nIl Bot � passato in Closed Beta il mese di ???????? ????, \nIl Bot � passato in Stable Relase il mese di ???????? ????")
+            .setTitle("Serve aiuto?")
+            .addField("❓ **__Cosa?__**", "**Arc-X** \u00E8 il centro di MVO: non solo aggiunge funzionalità al nostro server Discord, ma gestisce anche tutto il sistema di MasterVerse Online!")
+            .addField("❔ **__Chi?__**", "Il creatore (e attuale gestore) del Bot \u00E8 `" + client.guilds.cache.get(client.config.GuildServerID).members.cache.get(client.config.aloneID).user.tag + "`")
+            .addField("❓ **__Altro?__**", "Il Bot \u00E8 passato in Pre-Alpha nel mese di Gennaio 2021. \nIl Bot \u00E8 passato in Open Beta nel mese di Dicembre 2021.")
             .setFooter("Versione : " + client.config.version)
             .setColor("#4dff4d")
     } else {
         embed = new Discord.MessageEmbed()
-            .setTitle("Need a little Help?")
-            .addField("What?", "**AW | Project X** is the center of MVO, not only it works in the main discord server but it also manages the guilds system")
-            .addField("Who?", "The Bot was created and is managed by `" + client.guilds.cache.get(client.config.GuildServerID).members.cache.get(client.config.aloneID).user.tag + "`")
-            .addField("More?", "These infos were written in January 2021 and the Bot was in Pre Alpha, \nThe Bot was upgraded to Closed Beta in ??????? ????, \nThe Bot was upgraded in Stable Relase in ??????? ????")
+            .setTitle("Need help ?")
+            .addField("❓ **__What?__**", "**Arc-X** is the center of MVO: it not only adds functionality to our Discord server, but it also manages the entire MasterVerse Online system!")
+            .addField("❔ **__Who?__**", "The creator (and current manager) of the Bot is `" + client.guilds.cache.get(client.config.GuildServerID).members.cache.get(client.config.aloneID).user.tag + "`")
+            .addField("❓ ** __Other ? __ **", "The Bot moved into Pre-Alpha in January 2021. \nThe Bot moved into Open Beta in December 2021")
             .setFooter("Version : " + client.config.version)
             .setColor("#4dff4d")
     }
-    message.channel.send(embed)
+    message.channel.send({ embeds: [embed] }).catch(console.error)
 }
